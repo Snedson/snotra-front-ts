67018aa68228649ac1327886d8ff2c00baa96773
Lev Kochnev <75497303+thatslk@users.noreply.github.com>
2023-02-27 23:45:30 +0500

    installed and configured required dependencies

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,9 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import VueCookies from 'vue-cookies'
+import VueMaterialAdapter from 'vue-material-adapter'
 
-createApp(App).use(store).use(router).mount('#app')
+const app = createApp(App)
+
+app.use(store).use(router).use(VueCookies).use(VueMaterialAdapter).mount('#app')
