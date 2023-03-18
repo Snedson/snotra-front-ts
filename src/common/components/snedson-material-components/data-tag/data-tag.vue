7e145cae92f5cc875c15848654fe98ae08715e0b
Lev Kochnev <75497303+thatslk@users.noreply.github.com>
2023-03-19 04:36:48 +0500

    refactored data-tag component

--- src/common/components/snedson-material-components/data-tag/data-tag.vue
+++ src/common/components/snedson-material-components/data-tag/data-tag.vue
@@ -1,62 +1,60 @@
 <template>
-  <div class="data-tag">
-    <div class="data-tag__wrapper" :style="`background: ${color}`">
-      <img
-        v-if="iconLocal"
-        class="data-tag__icon"
-        :src="require(`@/assets/icons/emojis/${iconLocal}`)"
-      />
-      <img v-else-if="iconCompleteUrl" :src="iconCompleteUrl" />
-      <span class="data-tag__title">{{ props.title }}</span>
+    <div class="data-tag-wrapper">
+        <div class="data-tag" :style="`background: ${color}`">
+        <img
+            v-if="iconLocal"
+            class="data-tag__icon"
+            :src="require(`@/assets/icons/emojis/${iconLocal}`)"
+        />
+        <img v-else-if="iconCompleteUrl" :src="iconCompleteUrl" />
+        <p class="data-tag__title">{{ props.title }}</p>
+        </div>
     </div>
-  </div>
 </template>
 
 <script setup lang="ts">
 import { defineProps } from 'vue';
 
 const props = defineProps({
-  iconLocal: String,
-  iconCompleteUrl: String,
-  title: String,
-  color: String,
+    iconLocal: String,
+    iconCompleteUrl: String,
+    title: String,
+    color: String,
 });
 </script>
 
 <style lang="scss" scoped>
 .data-tag {
-  height: auto;
-  width: auto;
-  min-height: 30px;
-
-  font-family: 'Fira Sans';
-  font-style: normal;
-
-  &__wrapper {
     padding: 4px 10px;
     box-sizing: border-box;
     border-radius: 7px;
-    border: 1px solid #857370;
+    border: 1px solid var(--snotra--sys--outline);
     display: flex;
     flex-direction: row;
     width: auto;
     gap: 10px;
     align-items: center;
     width: fit-content;
-  }
 
-  &__icon {
-    width: 23px;
-    height: 23px;
-  }
+    &-wrapper {
+        height: auto;
+        width: auto;
+        min-height: 30px;
+    }
 
-  &__title {
-    margin: 0;
-    width: auto;
-    font-weight: 400;
-    font-size: 15px;
-    word-break: break-all;
-    white-space: pre-wrap;
-  }
+    &__icon {
+        width: 23px;
+        height: 23px;
+    }
+
+    &__title {
+        margin: 0;
+        width: auto;
+        font-weight: 400;
+        font-size: 15px;
+        word-break: break-all;
+        white-space: pre-wrap;
+        color: var(--snotra--sys--on-surface-variant);
+    }
 }
 </style>
