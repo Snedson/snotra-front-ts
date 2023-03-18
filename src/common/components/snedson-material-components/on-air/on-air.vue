e0ddca2f9bc32cdc12784a0cdcb9873868451f7d
Lev Kochnev <75497303+thatslk@users.noreply.github.com>
2023-03-19 04:38:15 +0500

    refactored on-air component

--- src/common/components/snedson-material-components/on-air/on-air.vue
+++ src/common/components/snedson-material-components/on-air/on-air.vue
@@ -1,28 +1,30 @@
 <template>
-  <div class="on-air">
-    <div class="on-air__wrapper">
-      <span class="on-air__title">Сейчас идёт</span>
-      <img src="@/assets/img/on-air/icon.svg" class="on-air__icon" />
+    <div class="on-air-wrapper">
+        <div class="on-air">
+            <p class="on-air__title">Сейчас идёт</p>
+            <img src="@/assets/img/on-air/icon.svg" class="on-air__icon" />
+        </div>
     </div>
-  </div>
 </template>
 
 <script setup lang="ts"></script>
 
 <style scoped lang="scss">
 .on-air {
-  font-family: 'Fira Sans';
-  font-style: normal;
-  width: fit-content;
-
-  &__wrapper {
     padding: 0px 0px 0px 15px;
     border-radius: 50px;
-    background: rgba(255, 251, 255, 0.85);
+    background: var(--snotra--sys--background-85);
     display: flex;
     align-items: center;
     gap: 5px;
     width: fit-content;
-  }
+    
+    &__title {
+        color: var(--snotra--sys--on-background);
+    }
+
+    &-wrapper {
+        width: fit-content;
+    }
 }
 </style>
