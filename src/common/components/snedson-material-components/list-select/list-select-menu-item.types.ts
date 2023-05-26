6c94275031093119319522407540b66870b8cd5f
Lev Kochnev <75497303+thatslk@users.noreply.github.com>
2023-05-26 15:48:45 +0500

    created list select menu item

---
+++ src/common/components/snedson-material-components/list-select/list-select-menu-item.types.ts
@@ -0,0 +1,3 @@
+export interface IListSelectMenuItem {
+    title: string;
+}
---
+++ src/common/components/snedson-material-components/list-select/list-select-menu-item.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="list-menu-item">
+        <state-layer class="list-menu-item__state-layer">
+            <p class="list-menu-item__title">{{ props.title }}</p>
+        </state-layer>
+    </div>
+</template>
+
+<script lang="ts" setup>
+import { defineProps } from 'vue';
+import { IListSelectMenuItem } from './list-select-menu-item.types';
+import stateLayer from '@/common/components/helper-components/state-layer/state-layer.vue';
+
+defineProps<{
+    props: IListSelectMenuItem;
+}>();
+</script>
+
+<style lang="scss" scoped>
+.list-menu-item {
+    width: 100%;
+    height: 56px;
+    background-color: var(--snotra--sys--surface);
+
+    &__state-layer {
+        padding: 0 30px;
+        display: flex;
+        align-items: center;
+    }
+
+    &__title {
+        color: var(--snotra--sys--on-surface-variant);
+    }
+}
+</style>
