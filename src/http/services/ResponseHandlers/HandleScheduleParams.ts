a5f4f3501540e428dc8caedc1691b4c27152a8fd
Lev Kochnev <75497303+thatslk@users.noreply.github.com>
2023-05-28 02:16:11 +0500

    simplified http/models folder structire & other changes
    - renamed http/model folder to http/pageModels
    - moved handleScheduleParams method to separate file

---
+++ src/http/services/ResponseHandlers/HandleScheduleParams.ts
@@ -0,0 +1,21 @@
+import { MspListModel } from "@/models/ScheduleLists/MspListModel";
+
+const handleScheduleParams = (responseMsp: MspListModel) : MspListModel => {
+    if(responseMsp) {
+        localStorage.setItem('mspVersion', responseMsp.version);
+        localStorage.setItem(
+            'modifiedScheduleParams',
+            JSON.stringify(responseMsp.todayScheduleParams)
+        );
+        return responseMsp;
+    } else {
+        const localMspList = localStorage.getItem('modifiedScheduleParams');
+        return {
+            todayScheduleParams: JSON.parse(
+                localMspList ? localMspList : '[]'
+            ), version: ''
+        }
+    }
+}
+
+export default handleScheduleParams;
