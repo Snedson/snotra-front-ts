e13e19a76dcd14b40b73e2dc7ebf7176f123cbef
Lev Kochnev <75497303+thatslk@users.noreply.github.com>
2023-06-09 03:28:26 +0500

    made custom button adaptive

--- src/common/components/snedson-material-components/custom-button/custom-button.vue
+++ src/common/components/snedson-material-components/custom-button/custom-button.vue
@@ -9,16 +9,22 @@
     >
         <state-layer
             :class="[
-                'custom-button__state-layer', 
-                'custom-button__state-layer_size_' + props.size
+                'custom-button__state-layer',
+                'custom-button__state-layer_size_' + props.size,
             ]"
         >
             <google-material-icon
                 :props="{
-                    iconName: props.iconName
+                    iconName: props.iconName,
                 }"
             ></google-material-icon>
-            <span v-if="props.size === 'full'" class="custom-button__title">
+            <span
+                v-if="isTFullSize(props.size)"
+                :class="[
+                    'custom-button__title',
+                    'custom-button__title_size_' + props.size,
+                ]"
+            >
                 {{ props.innerText }}
             </span>
         </state-layer>
@@ -27,7 +33,7 @@
 
 <script setup lang="ts">
 import stateLayer from '../../helper-components/state-layer/state-layer.vue';
-import { IButtonProps } from './custom-button.types';
+import { IButtonProps, isTFullSize } from './custom-button.types';
 import { defineProps } from 'vue';
 import googleMaterialIcon from '../../helper-components/google-material-icon/google-material-icon.vue';
 
@@ -49,7 +55,9 @@ defineProps<{
         border-radius: inherit;
         display: flex;
         &_size {
-            &_full {
+            &_full,
+            &_full-resizable-to-icon-large,
+            &_full-resizable-to-icon-small {
                 gap: 8px;
                 padding: 10px 24px;
             }
@@ -86,10 +94,31 @@ defineProps<{
     }
 
     &_size {
-        &_full {
+        &_full,
+        &_full-resizable-to-icon-large,
+        &_full-resizable-to-icon-small {
             font-weight: 700;
             font-size: 20px;
             line-height: 24px;
+        }
+    }
+}
+
+@media (max-width: 1090px) {
+    .custom-button__state-layer_size {
+        &_full-resizable-to-icon-large {
+            padding: 10px 35px;
+        }
+
+        &_full-resizable-to-icon-small {
+            padding: 10px;
+        }
+    }
+
+    .custom-button__title_size {
+        &_full-resizable-to-icon-large,
+        &_full-resizable-to-icon-small {
+            display: none;
         }
     }
 }
