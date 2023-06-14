dbe184299a9d2716d2f314b61121bdce375f6d2e
Lev Kochnev <75497303+thatslk@users.noreply.github.com>
2023-06-14 09:38:13 +0500

    implemented all students subpage

--- src/views/pages/TeacherPages/ClassesPages/ClassPage/Subpages/AllStudentsSubpage/AllStudentsSubpage.vue
+++ src/views/pages/TeacherPages/ClassesPages/ClassPage/Subpages/AllStudentsSubpage/AllStudentsSubpage.vue
@@ -1,11 +1,41 @@
 <template>
-    <p>Teacher's All Students Subpage</p>
+    <div class="students-list">
+        <universal-card
+            v-for="student in state.data"
+            v-bind:key="student.studentId"
+            :props="{
+                type: 'outlined',
+                content: student.getFullName,
+            }"
+        >
+        </universal-card>
+    </div>
 </template>
 
 <script lang="ts" setup>
+import UniversalCard from '@/common/components/snedson-material-components/universal-card/universal-card.vue';
+import { Student } from '@/http/pageModels/teacherModels/classPage/AllStudentsResponseModel';
+import { AllStudentsService } from '@/http/services/TeacherServices/ClassPageServices/AllStudentsService';
+import router from '@/router';
+import { onMounted, reactive } from 'vue';
 
+const state = reactive<{ data: Student[] }>({
+    data: [],
+});
+
+onMounted(() => {
+    const classId = router.currentRoute.value.params.classId;
+
+    AllStudentsService.getAllStudents(+classId).then((response) => {
+        state.data = response.data.students;
+    });
+});
 </script>
 
-<style lang="sccs" scoped>
-
+<style lang="scss" scoped>
+.students-list {
+    display: flex;
+    flex-direction: column;
+    gap: 20px;
+}
 </style>
