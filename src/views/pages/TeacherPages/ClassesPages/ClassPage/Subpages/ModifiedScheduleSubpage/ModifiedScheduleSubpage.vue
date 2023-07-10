e3516959d361f257a38338ab1d60cda68a3a9b33
nikitandro <nik.vish.93@mail.ru>
2023-07-10 12:57:37 +0500

    deleted redutant function

--- src/views/pages/TeacherPages/ClassesPages/ClassPage/Subpages/ModifiedScheduleSubpage/ModifiedScheduleSubpage.vue
+++ src/views/pages/TeacherPages/ClassesPages/ClassPage/Subpages/ModifiedScheduleSubpage/ModifiedScheduleSubpage.vue
@@ -45,25 +45,6 @@ const state = reactive<{ data: GetModifiedScheduleSubpageResponseModel }>({
     },
 });
 
-const getWeekdayTitle = (weekday: WeekDays): string => {
-    switch (weekday) {
-        case WeekDays.Monday:
-            return 'Понедельник';
-        case WeekDays.Tuesday:
-            return 'Вторник';
-        case WeekDays.Wednesday:
-            return 'Среда';
-        case WeekDays.Thursday:
-            return 'Четверг';
-        case WeekDays.Friday:
-            return 'Пятница';
-        case WeekDays.Saturday:
-            return 'Суббота';
-        case WeekDays.Sunday:
-            return 'Воскресенье';
-    }
-};
-
 onMounted(() => {
     const classId = router.currentRoute.value.params.classId;
 
