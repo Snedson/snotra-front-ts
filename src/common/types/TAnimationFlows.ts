6e38cd81e80077bc76c5c2db472d3145f92635c0
Lev Kochnev <75497303+thatslk@users.noreply.github.com>
2023-07-14 15:41:27 +0500

    moved IAnimationFlow from separate file

--- src/common/types/TAnimationFlows.ts
+++ src/common/types/TAnimationFlows.ts
@@ -1,1 +1,9 @@
-export type TAnimationFlows = 'left-to-right' | 'right-to-left' | 'top-to-down' | 'down-to-top'
+export interface IAnimationFlow {
+    flow: TAnimationFlows;
+}
+
+export type TAnimationFlows =
+    | 'left-to-right'
+    | 'right-to-left'
+    | 'top-to-down'
+    | 'down-to-top';
