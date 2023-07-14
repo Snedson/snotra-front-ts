1b5874e8ce3bf60b647b8774f7acd6da182ac74e
Lev Kochnev <75497303+thatslk@users.noreply.github.com>
2023-07-14 15:51:03 +0500

    moved animation types to subfolder

--- src/services/useAnimations.ts
+++ src/services/useAnimations.ts
@@ -1,13 +1,17 @@
-import { TAnimationDirections } from "@/common/types/TAnimationDirections";
-import { TAnimationFlows } from "@/common/types/TAnimationFlows";
+import { TAnimationDirections } from '@/common/types/animation-types/TAnimationDirections';
+import { TAnimationFlows } from '@/common/types/animation-types/TAnimationFlows';
 
-export function HandleAnimation(direction: TAnimationDirections, oldId: number, newId: number) : TAnimationFlows {
-    switch(direction) {
+export function HandleAnimation(
+    direction: TAnimationDirections,
+    oldId: number,
+    newId: number
+): TAnimationFlows {
+    switch (direction) {
         case 'horizontal':
             return oldId < newId ? 'left-to-right' : 'right-to-left';
         case 'vertical':
             return oldId < newId ? 'top-to-down' : 'down-to-top';
-        default: 
-            return 'left-to-right'
+        default:
+            return 'left-to-right';
     }
 }
