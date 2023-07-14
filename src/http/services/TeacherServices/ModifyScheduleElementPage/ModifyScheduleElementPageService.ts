fde9109b7617b8f117bfeef8efff5bab8724ce66
Lev Kochnev <75497303+thatslk@users.noreply.github.com>
2023-07-14 16:31:21 +0500

    moved HandleScheduleParams to cache-service

--- src/http/services/TeacherServices/ModifyScheduleElementPage/ModifyScheduleElementPageService.ts
+++ src/http/services/TeacherServices/ModifyScheduleElementPage/ModifyScheduleElementPageService.ts
@@ -1,8 +1,11 @@
 import $api from '@/http/api';
 import { ModifySeRequestModel } from '@/http/pageModels/teacherModels/modifyScheduleElementPage/ModifySePageRequestModel';
 import { ModifySePageResponseModel } from '@/http/pageModels/teacherModels/modifyScheduleElementPage/ModifySePageResponseModel';
-import { ModifiedState, ModifySePageStateModel } from '@/http/pageModels/teacherModels/modifyScheduleElementPage/ModifySePageStateModel';
-import handleScheduleParams from '../../ResponseHandlers/HandleScheduleParams';
+import {
+    ModifiedState,
+    ModifySePageStateModel,
+} from '@/http/pageModels/teacherModels/modifyScheduleElementPage/ModifySePageStateModel';
+import handleScheduleParams from '@/services/caching-managers/schedule-params';
 
 const getModifyScheduleElementPage = (
     requestModel: ModifySeRequestModel,
@@ -22,22 +25,24 @@ const getModifyScheduleElementPage = (
     });
 };
 
-const handleOriginalState = (response:ModifySePageResponseModel) : ModifySePageResponseModel => {
+const handleOriginalState = (
+    response: ModifySePageResponseModel
+): ModifySePageResponseModel => {
     return {
         actualMse: response.actualMse,
         isClientMSEGuidDiffers: response.isClientMSEGuidDiffers,
         locationsList: response.locationsList,
-        todayScheduleParams: handleScheduleParams(
-            response.todayScheduleParams
-        )
-    }
-}
+        todayScheduleParams: handleScheduleParams(response.todayScheduleParams),
+    };
+};
 
-const handleModifiedState = (response:ModifySePageResponseModel) : ModifiedState => {
+const handleModifiedState = (
+    response: ModifySePageResponseModel
+): ModifiedState => {
     return {
         locationId: response.actualMse.locationId,
-        comments: response.actualMse.comments
-    }
-}
+        comments: response.actualMse.comments,
+    };
+};
 
 export default getModifyScheduleElementPage;
--- src/services/caching-managers/schedule-params/index.ts
+++ src/services/caching-managers/schedule-params/index.ts
@@ -1,7 +1,7 @@
-import { MspListModel } from "@/models/ScheduleLists/MspListModel";
+import { MspListModel } from '@/models/ScheduleLists/MspListModel';
 
-const handleScheduleParams = (responseMsp: MspListModel) : MspListModel => {
-    if(responseMsp) {
+const handleScheduleParams = (responseMsp: MspListModel): MspListModel => {
+    if (responseMsp) {
         localStorage.setItem('mspVersion', responseMsp.version);
         localStorage.setItem(
             'modifiedScheduleParams',
@@ -11,11 +11,10 @@ const handleScheduleParams = (responseMsp: MspListModel) : MspListModel => {
     } else {
         const localMspList = localStorage.getItem('modifiedScheduleParams');
         return {
-            todayScheduleParams: JSON.parse(
-                localMspList ? localMspList : '[]'
-            ), version: ''
-        }
+            todayScheduleParams: JSON.parse(localMspList ? localMspList : '[]'),
+            version: '',
+        };
     }
-}
+};
 
 export default handleScheduleParams;
