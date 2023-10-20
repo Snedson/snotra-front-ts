4404c2ff101fed024f4b929116f5dc488dc56ab5
Lev Kochnev <75497303+thatslk@users.noreply.github.com>
2023-10-21 01:48:05 +0500

    fixed token refreshment

--- src/http/api/index.ts
+++ src/http/api/index.ts
@@ -1,24 +1,47 @@
 import AuthService from '@/services/AuthService';
 import axios from 'axios';
+// ATTENTION: DO NOT REMOVE THIS LINE!
+axios.defaults.withCredentials = true;
+// It took me three hours to figure out that
+// setting "withCredentials" to true by default (option A)
+// and setting it in axios fetch arguments (option B) (like axios.post('www.example.com', {withCredentials: true}))
+// DOES NOT GIVE SAME RESULT!
+//
+// If you wanna fetch subdomain-b.example.com from subdomain-a.example.com with cookies
+// You should use option B. Option A won't send cookies.
+//
+// For more details:
+// https://stackoverflow.com/questions/46458011/set-cookie-from-subdomain-a-for-subdomain-b
+// https://stackoverflow.com/questions/40941118/axios-wont-send-cookie-ajax-xhrfields-does-just-fine
 
 const $api = axios.create({
     withCredentials: true,
-    baseURL: undefined,
+    baseURL: getBaseUrl(),
 });
 
 export const authURL = 'https://auth.snotra.site';
+
+$api.interceptors.request.use(
+    (config) => {
+        return config;
+    },
+    (error) => {
+        return Promise.reject(error);
+    }
+);
 
 $api.interceptors.response.use(
     (response) => {
         return response;
     },
     async (error) => {
+        const originalRequest = error.config;
+
         if (error.response.status === 401) {
             try {
                 await axios
                     .post(`${authURL}/auth-api/Refresh/SilentRefreshWeb`)
                     .then((response) => {
-                        const originalRequest = error.config;
                         return $api(originalRequest);
                     })
                     .catch((reason) => {
@@ -32,7 +55,7 @@ $api.interceptors.response.use(
         if (error.response.status === 404) {
             AuthService.logout();
         }
-        return Promise.reject(error);
+        return $api(originalRequest);
     }
 );
 
