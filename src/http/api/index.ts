bdcecce50d56e8e9b5a2e33b524acd0476508354
Lev Kochnev <75497303+thatslk@users.noreply.github.com>
2023-10-20 21:52:40 +0500

    implemented silent token refreshment

--- src/http/api/index.ts
+++ src/http/api/index.ts
@@ -1,38 +1,53 @@
-import axios, { AxiosResponse } from 'axios';
 import AuthService from '@/services/AuthService';
+import axios from 'axios';
 
 const $api = axios.create({
     withCredentials: true,
-    baseURL: 'https://mbousosh1.snotra.site',
+    baseURL: undefined,
 });
 
 export const authURL = 'https://auth.snotra.site';
 
-// $api.interceptors.request.use(
-//     async (config) => {
-//         const baseURL = getBaseUrl();
-//         const role = getRole();
-//         if (!baseURL || !role) {
-//             // AuthService.logout();
-//             return config;
-//         }
-//         config.baseURL = baseURL;
-//         return config;
-//     },
-//     (error) => Promise.reject(error)
-// );
+$api.interceptors.response.use(
+    (response) => {
+        return response;
+    },
+    async (error) => {
+        if (error.response.status === 401) {
+            try {
+                await axios
+                    .post(`${authURL}/auth-api/Refresh/SilentRefreshWeb`)
+                    .then((response) => {
+                        const originalRequest = error.config;
+                        return $api(originalRequest);
+                    })
+                    .catch((reason) => {
+                        AuthService.logout();
+                    });
+            } catch (refreshError) {
+                return Promise.reject(refreshError);
+            }
+        }
 
-// $api.interceptors.response.use(
-//     (response) => {
-//         return response;
-//     },
-//     () => {
-//         // AuthService.logout();
-//     }
-// );
+        if (error.response.status === 404) {
+            AuthService.logout();
+        }
+        return Promise.reject(error);
+    }
+);
 
-export function getBaseUrl(): string | null {
-    return `https://${localStorage.getItem('schoolSnotraDomain')}/`;
+export function getBaseUrl(): string | undefined {
+    const snotraDomain = localStorage.getItem('schoolSnotraDomain');
+
+    if (!snotraDomain) {
+        return undefined;
+    }
+
+    return `https://${snotraDomain}/`;
+}
+
+export function updateBaseUrl() {
+    $api.defaults.baseURL = getBaseUrl();
 }
 
 export function getRole(): string | null {
